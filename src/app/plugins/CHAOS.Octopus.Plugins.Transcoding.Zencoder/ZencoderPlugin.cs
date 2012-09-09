0c0f9bb77a0c15fe17d951b8f303e6d97b2ad94d
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-09-10 01:23:10 +0200

    Added Zencoder SDK Added Zencoder Plugin and test

---
+++ src/app/plugins/CHAOS.Octopus.Plugins.Transcoding.Zencoder/ZencoderPlugin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using Geckon.Octopus.Plugin.Core;
+using Geckon.Serialization.Xml;
+using Zen = Zencoder;
+
+namespace CHAOS.Octopus.Plugins.Transcoding.Zencoder
+{
+    public class ZencoderPlugin : APlugin
+    {
+        #region Properties
+
+        [Element("ZencoderApiKey")]
+        public string ZencoderApiKey { get; set; }
+
+        [Element("DestinationFilePath")]
+        public string DestinationFilePath { get; set; }
+
+        [Element("SourceFilePath")]
+        public string SourceFilePath { get; set; }
+
+        [Element("Label")]
+        public string Label { get; set; }
+
+        [Element("VideoBitrate")]
+        public int? VideoBitrate { get; set; }
+
+        [Element("AudioBitrate")]
+        public int? AudioBitrate { get; set; }
+
+        [Element("Width")]
+        public int Width { get; set; }
+
+        [Element("Height")]
+        public int Height { get; set; }
+
+
+
+        #endregion
+        #region Business
+
+        protected override void Execute()
+        {
+            base.Execute();
+            
+            var zencoder = new Zen.Zencoder(ZencoderApiKey, new Uri("https://app.zencoder.com/api/v2/"));
+            var outputs = new Zen.Output[]
+                              {
+                                  new Zen.Output
+                                      {
+                                          Label        = Label,
+                                          Url          = DestinationFilePath,
+                                          Width        = Width,
+                                          Height       = Height,
+                                          VideoBitrate = VideoBitrate,
+                                          AudioBitrate = AudioBitrate
+                                      }, 
+                              };
+
+            var response = zencoder.CreateJob(SourceFilePath, outputs, null, "eu-dublin", false, false);
+            var output   = response.Outputs.First();
+
+            for (var outputState = zencoder.JobProgress(output.Id).State; outputState != Zen.OutputState.Finished && outputState != Zen.OutputState.Cancelled && outputState != Zen.OutputState.Failed; outputState = zencoder.JobProgress(output.Id).State)
+            {
+                OperationProgress = zencoder.JobProgress(output.Id).Progress / 100.0;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/test/plugins/Geckon.Octopus.Plugins.Transcoding.Zencoder.Test/ZencoderPluginTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CHAOS.Octopus.Plugins.Transcoding.Zencoder;
+using Geckon.Events;
+using Geckon.Octopus.Plugin.Interface;
+using NUnit.Framework;
+using Zen = Zencoder;
+
+namespace Geckon.Octopus.Plugins.Transcoding.Zencoder.Test
+{
+    [TestFixture]
+    public class ZencoderPluginTest
+    {
+        [Test]
+        public void Should_Execute_ZencoderPlugin()
+        {
+            var plugin = new ZencoderPlugin
+                             {
+                                 ZencoderApiKey = "aa86ad3b004917c8fe52b54fc38b09dd",
+                                 Label = "somelabel",
+                                 DestinationFilePath = "s3://chaosdata/outtest.mp4",
+                                 Width = 480,
+                                 Height = 320,
+                                 SourceFilePath = "s3://chaosdata/test.mp4"
+                             };
+
+            plugin.ExecuteFailed += delegate(object sender, ObjectErrorEventArgs<IPlugin> e) { throw e.Exception; };
+            plugin.OperationProgressChanged += delegate(object sender, ObjectProgressEventArgs<IPlugin> eventArgs){System.Console.WriteLine(eventArgs.NewProgress);};
+            plugin.BeginExecute();
+        }
+    }
+}
