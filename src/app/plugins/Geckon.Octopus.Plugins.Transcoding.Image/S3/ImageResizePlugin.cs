ab6595b9b30fb887a90e492bb601b1ae3dbe9399
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-10-25 17:09:19 +0200

    Added S3 image processing

---
+++ src/app/plugins/Geckon.Octopus.Plugins.Transcoding.Image/S3/ImageResizePlugin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Amazon;
+using Amazon.S3;
+using Geckon.Graphics;
+using Geckon.Octopus.Plugins.BasePlugins;
+using Geckon.Serialization.Xml;
+
+namespace Geckon.Octopus.Plugins.Transcoding.Image.S3
+{
+    public class ImageResizePlugin : APluginExtendedFileOperation
+    {
+        #region Properties
+
+        [Element("AWSAccessKey")]
+        public string AWSAccessKey { get; set; }
+
+        [Element("AWSSecretKey")]
+        public string AWSSecretKey { get; set; }
+
+        
+
+        #endregion
+        #region Business Logic
+
+        protected override void Execute()
+        {
+            base.Execute();
+            //  s3://s3-eu-west-1.amazonaws.com/chaosdata/hobit.mp4
+
+            var regexMatch = Regex.Match(SourceFilePath,@"^s3://(.+)\.amazonaws\.com/(.+)/(.+)$");
+
+            var region                  = regexMatch.Groups[1].Value;
+            var bucketName              = regexMatch.Groups[2].Value;
+            var key                     = regexMatch.Groups[3].Value;
+            var tempSourceFilepath      = Path.Combine(TemporaryFilePath, Path.GetRandomFileName());
+            var tempDestinationFilepath = Path.Combine(TemporaryFilePath, Path.GetRandomFileName());
+
+            using( var s3 = AWSClientFactory.CreateAmazonS3Client( AWSAccessKey, AWSSecretKey, GetRegion(region) ) )
+            {
+                var request = new Amazon.S3.Model.GetObjectRequest
+                                  {
+                                      BucketName = bucketName,
+                                      Key        = key
+                                  };
+
+                using (var stream = s3.GetObject(request).ResponseStream)
+                using (var file   = new FileStream(tempSourceFilepath, FileMode.Create))
+                {
+                    stream.CopyTo(file);
+                }
+            }
+
+            new Imaging(tempSourceFilepath).Resize(10, 10, FillTypes.scale, 10, tempDestinationFilepath);
+
+
+        }
+
+        private static RegionEndpoint GetRegion(string region)
+        {
+            switch (region)
+            {
+                case "s3-eu-west-1":
+                    return RegionEndpoint.EUWest1;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/test/plugins/Geckon.Octopus.Plugins.Transcoding.Image.Test/S3/ImageResizePluginTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Geckon.Octopus.Plugins.Transcoding.Image.Test.S3
+{
+    [TestFixture]
+    public class ImageResizePluginTest
+    {
+        [Test]
+        public void Should_Extract_Using_Regex()
+        {
+            //  
+
+            var regexMatch = Regex.Match("s3://s3-eu-west-1.amazonaws.com/chaosdata/hobit.mp4", @"^s3://(.+)\.amazonaws\.com/(.+)/(.+)$");
+
+            var region     = regexMatch.Groups[1].Value;
+            var bucketName = regexMatch.Groups[2].Value;
+            var key        = regexMatch.Groups[3].Value;
+
+            Assert.AreEqual("s3-eu-west-1", region);
+            Assert.AreEqual("chaosdata", bucketName);
+            Assert.AreEqual("hobit.mp4", key);
+        }
+    }
+}
