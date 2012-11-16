80a64cdcd74368c8c2b1d024845d5c8ee8cf8f5f
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-11-16 13:38:32 +0100

    Uploaded images are not publicly readable Added progress

--- src/app/plugins/Geckon.Octopus.Plugins.Transcoding.Image/S3/ImageResizePlugin.cs
+++ src/app/plugins/Geckon.Octopus.Plugins.Transcoding.Image/S3/ImageResizePlugin.cs
@@ -2,13 +2,16 @@
 using System.IO;
 using System.Text.RegularExpressions;
 using Amazon;
+using Amazon.S3.Model;
 using Geckon.Graphics;
+using Geckon.Logging;
+using Geckon.Octopus.Plugin.Core;
 using Geckon.Octopus.Plugins.BasePlugins;
 using Geckon.Serialization.Xml;
 
 namespace Geckon.Octopus.Plugins.Transcoding.Image.S3
 {
-    public class ImageResizePlugin : APluginExtendedFileOperation
+    public class ImageResizePlugin : APlugin
     {
         #region Properties
 
@@ -17,6 +20,12 @@ namespace Geckon.Octopus.Plugins.Transcoding.Image.S3
 
         [Element("AWSSecretKey")]
         public string AwsSecretKey { get; set; }
+
+        [Element("DestinationFilePath")]
+        public string DestinationFilePath { get; set; }
+
+        [Element("SourceFilePath")]
+        public string SourceFilePath { get; set; }
 
         [Element("TempSourceFilepath")]
         public string TempSourceFilepath { get; set; }
@@ -39,57 +48,60 @@ namespace Geckon.Octopus.Plugins.Transcoding.Image.S3
         protected override void Execute()
         {
             base.Execute();
-            //  s3://s3-eu-west-1.amazonaws.com/chaosdata/hobit.mp4
+        //s3-eu-west-1.amazonaws.com/chaosdata/hobit.mp4
+            OperationProgress = 0.1;
+            var sourceS3Settings        = GetS3Settings(SourceFilePath); OperationProgress = 0.2;
+            var destinationS3Settings   = GetS3Settings(DestinationFilePath); OperationProgress = 0.3;
+            
+            TempSourceFilepath      = Path.ChangeExtension(Path.GetRandomFileName(), Path.GetExtension(DestinationFilePath)); OperationProgress = 0.4;
+            TempDestinationFilepath = Path.ChangeExtension(Path.GetRandomFileName(), Path.GetExtension(DestinationFilePath)); OperationProgress = 0.5;
 
-            var sourceS3Settings        = GetS3Settings(SourceFilePath);
-            var destinationS3Settings   = GetS3Settings(DestinationFilePath);
-
-            TempSourceFilepath      = Path.GetRandomFileName();
-            TempDestinationFilepath = Path.GetRandomFileName();
-   
             using (var s3 = AWSClientFactory.CreateAmazonS3Client(AwsAccessKey, AwsSecretKey, GetRegion(sourceS3Settings.Region)))
             {
+                OperationProgress = 0.6;
                 var request = new Amazon.S3.Model.GetObjectRequest
                                   {
                                       BucketName = sourceS3Settings.BucketName,
-                                      Key        = sourceS3Settings.Key
-                                  };
+                                      Key = sourceS3Settings.Key
+                                  }; OperationProgress = 0.7;
 
                 using (var stream = s3.GetObject(request).ResponseStream)
                 using (var file   = new FileStream(TempSourceFilepath, FileMode.Create))
                 {
+                    OperationProgress = 0.8;
                     stream.CopyTo(file);
                 }
+                OperationProgress = 0.9;
             }
 
-            RollbackLevel = ROLLBACK_LEVEL_FILE_OPERATION_BEGUN;
-
-            new Imaging(TempSourceFilepath).Resize(Width, Height, FillTypes.scale, Quality, TempDestinationFilepath);
-
-            RollbackLevel = ROLLBACK_LEVEL_TEMPORARY_FILE_CREATED;
-
+            using (var imaging = new Imaging(TempSourceFilepath))
+            {
+                imaging.Resize(Width, Height, FillTypes.scale, Quality, TempDestinationFilepath);
+            }
+            
+            OperationProgress = 0.9;
             using (var s3 = AWSClientFactory.CreateAmazonS3Client(AwsAccessKey, AwsSecretKey, GetRegion(destinationS3Settings.Region)))
             {
+                OperationProgress = 0.10;
                 var request = new Amazon.S3.Model.PutObjectRequest
                                   {
                                       BucketName      = destinationS3Settings.BucketName,
                                       Key             = destinationS3Settings.Key,
                                       FilePath        = TempDestinationFilepath,
                                       AutoCloseStream = true,
-                                      Timeout         = 30*60*60
+                                      Timeout         = 30 * 60 * 60,
+                                      CannedACL       = S3CannedACL.PublicRead
                                   };
-
+                OperationProgress = 0.11;
                 s3.PutObject(request);
             }
-
-            RollbackLevel = ROLLBACK_LEVEL_FILE_OPERATION_ENDED;
         }
 
         protected override void Commit()
         {
             base.Commit();
 
-            if(File.Exists(TempSourceFilepath))
+            if (File.Exists(TempSourceFilepath))
                 File.Delete(TempSourceFilepath);
 
             if (File.Exists(TempDestinationFilepath))
@@ -109,7 +121,7 @@ namespace Geckon.Octopus.Plugins.Transcoding.Image.S3
 
         private static S3Settings GetS3Settings(string s3Path)
         {
-            var regexMatch = Regex.Match(s3Path, @"^s3://(.+)\.amazonaws\.com/(.+)/(.+)$");
+            var regexMatch = Regex.Match(s3Path, @"^http[s]*://(.+)\.amazonaws\.com/(.+?)/(.+)$");
 
             return new S3Settings
                        {
